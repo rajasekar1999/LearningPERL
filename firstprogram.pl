ae9fb0a80e797b410eb94db53179b51fb64d4588
Rajasekar M <36572895+rajasekar1999@users.noreply.github.com>
2018-07-30 22:24:41 +0530

    Add files via upload

---
+++ firstprogram.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+
+system("clear");
+print "Hello World!\n";
+print "\nEnter the first number: \n";
+$a = <STDIN>;
+print "Enter the second number: \n";
+$b = <STDIN>;
+print "\n",($a * $b),"\n";
+print "\nThere's the answer...\n"
